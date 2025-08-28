63145e45f452cae9193416d03e4625773ba61590
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2025-08-28 22:15:25 +0530

    Service Discovery

---
+++ api-gateway/src/main/java/com/microservices/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.microservices.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
