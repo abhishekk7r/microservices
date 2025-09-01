082f1a66ebb2163685b1ca5591ba4ae426a56999
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2025-09-01 20:37:23 +0530

    Commit For API Gateway Integration

--- api-gateway/src/main/java/com/microservices/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/microservices/api_gateway/ApiGatewayApplication.java
@@ -3,11 +3,6 @@ package com.microservices.api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
-
-
-@EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
