4d82056d7293a0524917c4c5f331a74a7036eda2
Abhishek Kumar <73238301+abhishekk7r@users.noreply.github.com>
2025-10-06 15:40:39 +0530

    Integrated Kafka

--- order_service/src/main/java/com/microservices/orderservice/service/OrderService.java
+++ order_service/src/main/java/com/microservices/orderservice/service/OrderService.java
@@ -1,13 +1,12 @@
 package com.microservices.orderservice.service;
 
-import com.microservices.orderservice.dto.InventoryRequest;
-import com.microservices.orderservice.dto.InventoryResponse;
-import com.microservices.orderservice.dto.OrderLineItemsDto;
-import com.microservices.orderservice.dto.OrderRequest;
+import com.microservices.orderservice.dto.*;
 import com.microservices.orderservice.model.Order;
 import com.microservices.orderservice.model.OrderLineItems;
 import com.microservices.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,10 +17,12 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         // Create order with random ID
@@ -41,15 +42,20 @@ public class OrderService {
                 .map(item -> new InventoryRequest(item.getSkuCode(), item.getQuantity()))
                 .toList();
 
-        boolean stockAvailable = webClientBuilder.build().post()
+        boolean stockAvailable = Boolean.TRUE.equals(webClientBuilder.build().post()
                 .uri("http://inventory-service/api/inventory/check")
                 .bodyValue(requests)
                 .retrieve()
                 .bodyToMono(Boolean.class)
-                .block();
+                .block());
 
         if (stockAvailable) {
+            log.info("Before Saving Order Repo + {}", order.getOrderNumber());
             orderRepository.save(order);
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            kafkaTemplate.send("notificationTopic", orderPlacedEvent);
+            log.info("Processing order number + {}", order.getOrderNumber());
         } else {
             throw new IllegalArgumentException("One or more products are out of stock, please try again later");
         }
